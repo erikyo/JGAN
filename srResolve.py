0dc04212b5ab644a836f87d10b413d5480b21339
Erik Golinelli <erik@codekraft.it>
2022-08-15 17:19:09 +0200

    better image chunking

--- srResolve.py
+++ srResolve.py
@@ -14,8 +14,6 @@ gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
 
-
-
 pre_generator = generator()
 gan_generator = generator()
 
@@ -31,12 +29,13 @@ def sliceImage(image, tiles_x_size=48, tiles_y_size=48):
     return [image[x:x + tiles_x_size, y:y + tiles_y_size] for x in range(0, image.shape[0], tiles_x_size) for y in
             range(0, image.shape[1], tiles_y_size)]
 
-def resolve_and_plot(lr_image_path, tile_size=48):
+def resolve_and_plot(lr_image_path, tile_size=96):
 
     lr = Image.open(lr_image_path)
     width, height = lr.size
     x_tiles, y_tiles = round(width/tile_size)+1, round(height/tile_size)
-    print("Image split into", x_tiles*y_tiles,":", x_tiles, "X tiles and", y_tiles, "Y tiles of", tile_size, "px")
+    print("Image size", width, "x", height)
+    print("Image split into", x_tiles*y_tiles,":", x_tiles, "X tiles and", y_tiles, "Y (", tile_size, "px tiles)")
     lr_slices = sliceImage(np.array(lr))
 
     processed_slices = [];
@@ -48,18 +47,17 @@ def resolve_and_plot(lr_image_path, tile_size=48):
 
         pil_img = tf.keras.preprocessing.image.array_to_img(gan_sr)
         processed_slices.append(pil_img)
-        filename= os.path.splitext(lr_image_path)[0] + "-" + str(increment) + ".png"
-        print(filename)
-        increment = increment+1
-        pil_img.save(filename)
 
-    new_im = Image.new('RGB', (width, height))
+    new_im = Image.new('RGB', (width * 4, height * 4))
     w, h = new_im.size
-    for i in range(0, w, tile_size):
-        for j in range(0, h, tile_size):
-            new_im.paste(processed_slices, (i, j))
+    index = 0
+    big_tile_size = tile_size * 4;
+    for j in range(0, h, big_tile_size):
+        for i in range(0, w, big_tile_size):
+            new_im.paste(processed_slices[index], (i, j))
+            index = index + 1
 
-    new_im.save(os.path.join(OUTPUT_DIR, file))
+    filename= os.path.splitext(lr_image_path)[0] + "-res" + ".png"
+    new_im.save(filename)
 
-
-resolve_and_plot("demo/gibox.jpg")
+resolve_and_plot("demo/image.jpg")
